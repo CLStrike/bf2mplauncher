70790c4962829917b23b40643256a9dabdc56c5e
Coen <Coen@DESKTOP-IRU7MS1>
2021-07-13 14:00:01 +0200

    Add project files.

---
+++ Battlefield 2 Multiplayer/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Battlefield_2_Multiplayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.DragPanel = new System.Windows.Forms.Panel();
+            this.version_label = new System.Windows.Forms.Label();
+            this.shorcut_btn = new System.Windows.Forms.Button();
+            this.server_address_field = new System.Windows.Forms.TextBox();
+            this.connect_btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.minimize_btn = new System.Windows.Forms.Button();
+            this.close_btn = new System.Windows.Forms.Button();
+            this.DragPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DragPanel
+            // 
+            this.DragPanel.BackColor = System.Drawing.Color.Transparent;
+            this.DragPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.DragPanel.Controls.Add(this.version_label);
+            this.DragPanel.Controls.Add(this.shorcut_btn);
+            this.DragPanel.Controls.Add(this.server_address_field);
+            this.DragPanel.Controls.Add(this.connect_btn);
+            this.DragPanel.Controls.Add(this.pictureBox1);
+            this.DragPanel.Controls.Add(this.minimize_btn);
+            this.DragPanel.Controls.Add(this.close_btn);
+            this.DragPanel.Location = new System.Drawing.Point(0, 1);
+            this.DragPanel.MaximumSize = new System.Drawing.Size(930, 464);
+            this.DragPanel.MinimumSize = new System.Drawing.Size(930, 464);
+            this.DragPanel.Name = "DragPanel";
+            this.DragPanel.Size = new System.Drawing.Size(930, 464);
+            this.DragPanel.TabIndex = 0;
+            this.DragPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.DragPanel_Paint);
+            this.DragPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DragPanel_MouseDown);
+            this.DragPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DragPanel_MouseMove);
+            this.DragPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.DragPanel_MouseUp);
+            // 
+            // version_label
+            // 
+            this.version_label.AutoSize = true;
+            this.version_label.Location = new System.Drawing.Point(862, 442);
+            this.version_label.Name = "version_label";
+            this.version_label.Size = new System.Drawing.Size(59, 13);
+            this.version_label.TabIndex = 5;
+            this.version_label.Text = "version 1.2";
+            // 
+            // shorcut_btn
+            // 
+            this.shorcut_btn.BackColor = System.Drawing.SystemColors.Control;
+            this.shorcut_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.shorcut_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.shorcut_btn.Location = new System.Drawing.Point(175, 347);
+            this.shorcut_btn.Name = "shorcut_btn";
+            this.shorcut_btn.Size = new System.Drawing.Size(184, 39);
+            this.shorcut_btn.TabIndex = 4;
+            this.shorcut_btn.Text = "Create shortcut";
+            this.shorcut_btn.UseVisualStyleBackColor = false;
+            this.shorcut_btn.Click += new System.EventHandler(this.shorcut_btn_Click);
+            // 
+            // server_address_field
+            // 
+            this.server_address_field.AutoCompleteCustomSource.AddRange(new string[] {
+            "127.0.0.1",
+            "localhost"});
+            this.server_address_field.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.server_address_field.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.server_address_field.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.server_address_field.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.server_address_field.Location = new System.Drawing.Point(59, 290);
+            this.server_address_field.MinimumSize = new System.Drawing.Size(250, 40);
+            this.server_address_field.Name = "server_address_field";
+            this.server_address_field.Size = new System.Drawing.Size(300, 32);
+            this.server_address_field.TabIndex = 1;
+            this.server_address_field.Text = "server address here...";
+            this.server_address_field.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.server_address_field.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.server_address_field.KeyDown += new System.Windows.Forms.KeyEventHandler(this.server_address_field_KeyDown);
+            this.server_address_field.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.server_address_field_KeyPress);
+            // 
+            // connect_btn
+            // 
+            this.connect_btn.BackColor = System.Drawing.SystemColors.Control;
+            this.connect_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connect_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.connect_btn.Location = new System.Drawing.Point(59, 347);
+            this.connect_btn.Name = "connect_btn";
+            this.connect_btn.Size = new System.Drawing.Size(110, 39);
+            this.connect_btn.TabIndex = 2;
+            this.connect_btn.Text = "Connect";
+            this.connect_btn.UseVisualStyleBackColor = false;
+            this.connect_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Battlefield_2_Multiplayer.Properties.Resources._1665_1_;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // minimize_btn
+            // 
+            this.minimize_btn.BackColor = System.Drawing.Color.Transparent;
+            this.minimize_btn.FlatAppearance.BorderSize = 0;
+            this.minimize_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimize_btn.Image = global::Battlefield_2_Multiplayer.Properties.Resources.minimize2;
+            this.minimize_btn.Location = new System.Drawing.Point(865, 3);
+            this.minimize_btn.Name = "minimize_btn";
+            this.minimize_btn.Size = new System.Drawing.Size(28, 23);
+            this.minimize_btn.TabIndex = 2;
+            this.minimize_btn.UseVisualStyleBackColor = false;
+            this.minimize_btn.Click += new System.EventHandler(this.minimize_btn_Click);
+            // 
+            // close_btn
+            // 
+            this.close_btn.BackColor = System.Drawing.Color.Transparent;
+            this.close_btn.FlatAppearance.BorderSize = 0;
+            this.close_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.close_btn.Image = global::Battlefield_2_Multiplayer.Properties.Resources.close_btn;
+            this.close_btn.Location = new System.Drawing.Point(899, 3);
+            this.close_btn.Name = "close_btn";
+            this.close_btn.Size = new System.Drawing.Size(28, 23);
+            this.close_btn.TabIndex = 1;
+            this.close_btn.UseVisualStyleBackColor = false;
+            this.close_btn.Click += new System.EventHandler(this.close_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Battlefield_2_Multiplayer.Properties.Resources.Application_background;
+            this.ClientSize = new System.Drawing.Size(930, 465);
+            this.ControlBox = false;
+            this.Controls.Add(this.DragPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragPanel.ResumeLayout(false);
+            this.DragPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel DragPanel;
+        private System.Windows.Forms.Button close_btn;
+        private System.Windows.Forms.Button minimize_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button connect_btn;
+        private System.Windows.Forms.TextBox server_address_field;
+        private System.Windows.Forms.Button shorcut_btn;
+        private System.Windows.Forms.Label version_label;
+    }
+}
+
---
+++ Battlefield 2 Multiplayer/Form1.cs
@@ -0,0 +1,446 @@
+﻿using IWshRuntimeLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Cache;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.Windows.Forms;
+namespace Battlefield_2_Multiplayer
+{
+
+    //TODO: Add custom path of BF2 to config file for later easy launch!
+    public partial class Form1 : Form
+    {
+        bool mouseDown;
+        private Point offset;
+        private readonly string Version = "1.4";
+        [DllImport("WinInet.dll", PreserveSig = true, SetLastError = true)]
+        public static extern void DeleteUrlCacheEntry(string url);
+        public Form1()
+        {
+            InitializeComponent();
+            GetSetVersion();
+            GetIPS();
+            GetServerHistory();
+            CheckforUpdate();
+        }
+
+        private void GetServerHistory()
+        {
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("serverhistory.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the lie to console window
+                    Console.WriteLine(line);
+                    server_address_field.AutoCompleteCustomSource.Add(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Done adding history");
+            }
+        }
+
+        private void GetIPS()
+        {
+            server_address_field.AutoCompleteCustomSource.Add(Environment.MachineName); // ADD machine hostname
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 || ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                {
+                    foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            server_address_field.AutoCompleteCustomSource.Add(ip.Address.ToString());
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public string GetSetVersion()
+        {
+            version_label.Text = "Version: " + Version;
+            return Version;
+
+        }
+
+        private void CheckforUpdate()
+        {
+            //initialize variables.
+            string URL = "https://raw.githubusercontent.com/CLStrike/bf2mplauncher/main/";
+            string AppName = "BF2";
+            string ServerVersion;
+            string serverVersionName = "Update.txt";
+            DeleteUrlCacheEntry(URL + serverVersionName);
+            //setting up policies
+            HttpRequestCachePolicy policy = new HttpRequestCachePolicy(HttpRequestCacheLevel.Default);
+            HttpWebRequest.DefaultCachePolicy = policy;
+            WebRequest req = WebRequest.Create(URL + serverVersionName);
+            HttpRequestCachePolicy noCachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.NoCacheNoStore);
+            req.CachePolicy = noCachePolicy;
+            WebResponse res = req.GetResponse();
+            Stream str = res.GetResponseStream();
+            StreamReader tr = new StreamReader(str);
+            ServerVersion = tr.ReadLine();
+
+
+            if (GetSetVersion() != ServerVersion)
+            {
+                //Update
+                string appnameversion = AppName + "-" + ServerVersion + ".zip";
+                System.Console.WriteLine(appnameversion);
+                DialogResult dialogResult =  MessageBox.Show("There is an update available!\n Do you want to download it?", "BF2 MP Launcher - Auto Updater", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    WebClient client = new WebClient();
+                    byte[] appdata = client.DownloadData(URL + appnameversion);
+                    using (FileStream fs = System.IO.File.Create(appnameversion))
+                    {
+                        fs.Write(appdata, 0, appdata.Length);
+                    }
+
+                    if(System.IO.File.Exists(Path.Combine(Path.GetTempPath(), "Battlefield 2 Multiplayer Launcher.exe")))
+                    {
+                        System.IO.File.Delete(Path.Combine(Path.GetTempPath(), "Battlefield 2 Multiplayer Launcher.exe"));
+
+                    }
+                    System.IO.Compression.ZipFile.ExtractToDirectory(appnameversion, Path.GetTempPath()); 
+                    System.IO.File.Copy(Path.Combine(Path.GetTempPath(), "Battlefield 2 Multiplayer Launcher.exe"), Path.Combine(Directory.GetCurrentDirectory(), "Battlefield 2 Multiplayer Launcher1.exe"), true);
+
+                    var p = new Process();
+                    p.StartInfo.FileName = "startupdate.bat";  // startupdatebat and kill own process.
+                    p.Start();
+
+                } else if (dialogResult == DialogResult.No)
+                {
+                    // nothing
+                }
+
+            }
+            else
+            {
+                if (System.IO.File.Exists("Battlefield 2 Multiplayer Launcher1.exe"))
+                {
+                    System.Console.WriteLine("Just done update, Cleaning up directory!");
+                    System.IO.File.Delete("Battlefield 2 Multiplayer Launcher1.exe");
+                    System.IO.File.Delete("BF2-1.4.zip");
+                }
+                else
+                {
+                    MessageBox.Show("No Update is Available!", "Auto Updater", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+            }
+
+        }
+            private void DragPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void DragPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            offset.X = e.X;
+            offset.Y = e.Y;
+            mouseDown = true;
+
+        }
+
+        private void DragPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown == true)
+            {
+                Point currentScreenPos = PointToScreen(e.Location);
+
+                Location = new Point(currentScreenPos.X - offset.X, currentScreenPos.Y - offset.Y);
+            }
+        }
+
+        private void DragPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+
+        }
+
+        private void close_btn_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void minimize_btn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private string Find_installationPath()
+        {
+            string[] defaultprogramlist = { "c:\\2\\BF2.exe", "c:\\3\\BF2.exe", "C:\\Program Files (x86)\\Bull3t\\BFHD\\BF2.exe" };
+            foreach (string i in defaultprogramlist)
+            {
+
+                if (System.IO.File.Exists(i))
+                {
+                    System.Console.WriteLine("File exists... " + i);
+                    // Doing input validation
+                    return i;
+
+                }
+                else
+                {
+                    System.Console.WriteLine("File not found: " + i);
+                }
+            }
+            System.Console.WriteLine("Nothing found!!");
+            return null;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Default program locations:
+            string[] defaultprogramlist = {  "c:\\2\\BF2.exe", "c:\\3\\BF2.exe" , "C:\\Program Files (x86)\\Bull3t\\BFHD\\BF2.exe" };
+            string[] serveraddressport;
+            string serverport;
+            string serveraddr;
+            char delim = ':';
+            string chosen_execpath;
+            string workingdirectory;
+            bool filefound = false;
+
+
+            foreach ( string i in defaultprogramlist){ 
+
+                if (System.IO.File.Exists(i))
+                {
+                    System.Console.WriteLine("File exists... " + i);
+                    filefound = true;
+                    // Doing input validation
+                    chosen_execpath = i;
+
+                    if (server_address_field.Text.Contains(delim))
+                    {
+                        serveraddressport = server_address_field.Text.Split(':');
+                        serveraddr = serveraddressport[0];
+                        serverport = serveraddressport[1];
+                    }
+                    else
+                    {
+                        serveraddr = server_address_field.Text;
+                        serverport = "16567";
+                    }
+
+                    WriteServerAddrToFile(serveraddr);
+                    // Connect to server
+                    workingdirectory = chosen_execpath.Replace("BF2.exe", "");
+                    Process runbf2 = new Process();
+                    runbf2.StartInfo.FileName = chosen_execpath;
+                    runbf2.StartInfo.Arguments = " +joinServer " + serveraddr + " +port " + serverport;
+                    runbf2.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+                    runbf2.StartInfo.WorkingDirectory = workingdirectory;
+                    System.Console.WriteLine("DEBUG - Selected Working directory: " + workingdirectory);
+                    System.Console.WriteLine("DEBUG - command used: " + chosen_execpath + " +joinServer " + serveraddr + " +port " + serverport);
+
+                    runbf2.Start();
+                }
+                else
+                {
+                    System.Console.WriteLine("File not found: " + i);
+                }
+            }
+            if(filefound == false) {
+
+                System.Console.WriteLine("Nothing found!");
+                OpenFileDialog openfile = new OpenFileDialog();
+                openfile.InitialDirectory = @"C:\";
+                openfile.Title = "Select BF2.exe";
+                openfile.DefaultExt = ".exe";
+                openfile.CheckFileExists = true;
+                openfile.CheckPathExists = true;
+                openfile.ShowDialog();
+
+                if (openfile.ShowDialog() == DialogResult.OK)
+                {
+                    chosen_execpath = openfile.FileName;
+                    if (server_address_field.Text.Contains(delim))
+                    {
+                        serveraddressport = server_address_field.Text.Split(':');
+                        serveraddr = serveraddressport[0];
+                        serverport = serveraddressport[1];
+                    }
+                    else
+                    {
+                        serveraddr = server_address_field.Text;
+                        serverport = "16567";
+                    }
+
+                    // Connect to server
+                    workingdirectory = chosen_execpath.Replace("BF2.exe", "");
+                    Process runbf2 = new Process();
+                    runbf2.StartInfo.FileName = chosen_execpath;
+                    runbf2.StartInfo.Arguments = " +joinServer " + serveraddr + " +port " + serverport;
+                    runbf2.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+                    runbf2.StartInfo.WorkingDirectory = workingdirectory;
+                    System.Console.WriteLine("DEBUG - Selected Working directory: " + workingdirectory);
+                    System.Console.WriteLine("DEBUG - command used: " + chosen_execpath + " +joinServer " + serveraddr + " +port " + serverport);
+
+                    runbf2.Start();
+                }
+
+            }
+
+
+        }
+
+        private void WriteServerAddrToFile(string serveraddr)
+        {
+            try
+            {
+                if (System.IO.File.Exists("serverhistory.txt")){
+
+
+                    StreamWriter sw = new StreamWriter("serverhistory.txt", true);
+                    //Write a line of text
+                    sw.WriteLine(serveraddr);
+                    //Write a second line of text
+                    //Close the file
+                    sw.Close();
+                }
+                else
+                {
+                    StreamWriter sw = new StreamWriter("serverhistory.txt");
+                    //Write a line of text
+                    sw.WriteLine(serveraddr);
+                    //Write a second line of text
+                    //Close the file
+                    sw.Close();
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+        private void shorcut_btn_Click(object sender, EventArgs e)
+        {
+            string[] serveraddressport;
+            string serverport;
+            string serveraddr;
+            string Shortcut_name;
+            string ROOTPATH;
+            string targetPath;
+            string command;
+            string targetcommand;
+
+
+            char delim = ':';
+            if (server_address_field.Text.Contains(delim))
+            {
+                serveraddressport = server_address_field.Text.Split(':');
+                serveraddr = serveraddressport[0];
+                serverport = serveraddressport[1];
+            }
+            else
+            {
+                serveraddr = server_address_field.Text;
+                serverport = "16567";
+            }
+            // System.Console.WriteLine(server_address); //For troubleshooting purposes.
+            ROOTPATH = Find_installationPath().Replace("BF2.exe ", "");
+            Shortcut_name = "BF2 MP " + serveraddr;
+            targetPath = ROOTPATH;
+            command = " +joinServer " + serveraddr + " +port " + serverport;
+            targetcommand = targetPath;
+
+
+            CreateShortcut(Shortcut_name, Environment.GetFolderPath(Environment.SpecialFolder.Desktop), targetcommand, command, ROOTPATH);
+            System.Console.WriteLine("Shortcut created " + Shortcut_name);
+        }
+        public static void CreateShortcut(string shortcutName, string shortcutPath, string targetFileLocation, string Arguments, string Workdir)
+        {
+            string shortcutLocation = System.IO.Path.Combine(shortcutPath, shortcutName + ".lnk");
+            string startupPath = AppDomain.CurrentDomain.BaseDirectory;
+            string targetPath = startupPath + "Resources\\";
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
+
+            shortcut.Description = "BF2 Shortcut to Server";   // The description of the shortcut
+            shortcut.IconLocation = targetPath + "BF2 MP icon.ico";           // The icon of the shortcut
+            shortcut.TargetPath = targetFileLocation;                 // The path of the file that will launch when the shortcut is run
+            shortcut.Arguments = Arguments;
+            shortcut.WorkingDirectory = Workdir.Replace("BF2.exe", " ");
+            shortcut.Save();                                    // Save the shortcut
+        }
+
+        private void server_address_field_KeyDown(object sender, KeyEventArgs e)
+        {
+            //if (e.KeyCode == Keys.Enter)
+            //{
+            //    button1_Click(this, new EventArgs());
+            //}
+        }
+
+        private void server_address_field_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar)
+               && !char.IsDigit(e.KeyChar)
+               && !char.IsLetter(e.KeyChar)
+               && e.KeyChar != ':' && e.KeyChar != '-' && e.KeyChar != '.')
+            {
+                e.Handled = true;
+                return;
+            }
+            e.Handled = false;
+            return;
+        }
+    }
+}
